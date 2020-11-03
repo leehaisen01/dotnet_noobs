c82e30aa6acf1b814727e36f07d9f23fbd20c07f
Lee Haisen <leehaisen01@gmail.com>
2020-11-03 19:44:43 +0500

    Intro To Types

---
+++ GeekTraining/01_Intro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01_Intro {
+    class Program {
+        static void Main(string[] args) {
+            #region Introduction to Types
+            // data types
+            int x = 1;
+            short s = 21345;
+            float f = 12.222222f;
+            double dbl = 12342;
+            decimal ds = 12.2222222222222222222m;
+            long l = 1238921;
+
+            char c = 'a';
+            string str = "This is a long long paragraph";
+
+            bool iamyoung = true;
+            DateTime today = DateTime.Now;
+
+
+            Console.WriteLine("Hello World!");
+            #endregion
+        }
+    }
+}
