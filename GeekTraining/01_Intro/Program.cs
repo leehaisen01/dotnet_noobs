c89178df7430791f4499bb1239e9b1101d03ee4a
Lee Haisen <leehaisen01@gmail.com>
2020-11-08 18:24:39 +0500

    Arrays, For Loop & Conditionals

--- GeekTraining/01_Intro/Program.cs
+++ GeekTraining/01_Intro/Program.cs
@@ -17,9 +17,62 @@ namespace _01_Intro {
 
             bool iamyoung = true;
             DateTime today = DateTime.Now;
+            #endregion
+
+            #region Arrays, For Loop & Conditionals
+            int n1 = 1;
+            int n2 = 2;
+            int n3 = 3;
+            int n4 = 4;
+            int n5 = 5;
+            int n6 = 6;
+
+            // arrays
+            int[] bottles = new int[10]; // array
+            bottles[0] = 1;
+            bottles[1] = 10;
+            bottles[2] = 500;
+            bottles[3] = 800;
+            bottles[4] = 800;
+            bottles[5] = 800;
+            bottles[6] = 800;
+
+            Console.WriteLine(bottles.Length);
+
+            // for loop
+            for (int i = 0; i < bottles.Length; i++) {
+                Console.WriteLine($"Bottle at Index[{i}] has value = {bottles[i]}");
+                //Console.WriteLine("Bottle at Index[" + i + "] has value = " + bottles[i] + "");
+            }
+
+            // if else statment
+            int age = 17;
+            if (age > 18) {
+                Console.WriteLine("Welcome To Website.");
+            }
+            else {
+                Console.WriteLine("Sorry you are too young.");
+            }
 
 
-            Console.WriteLine("Hello World!");
+            if (age > 18) {
+                Console.WriteLine("Welcome To Website.");
+            }
+            else if(age == 17) {
+                Console.WriteLine("You have to wait one year for this.");
+            }
+            else {
+                Console.WriteLine("Sorry you are too young.");
+            }
+
+            // console input 
+            Console.Write("Enter A Number : ");
+            int val = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your Entered {val}");
+            // console output
+            Console.Write("This");
+            Console.Write(" is a sentence \n");
+            Console.WriteLine("This doesn't need any escape character");
             #endregion
         }
     }
